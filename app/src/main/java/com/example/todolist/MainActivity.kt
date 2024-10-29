bb9fe89a982e8733418b0b81e88abec631cbbd4b
wnsdud <cccjj3829@gmail.com>
2024-10-29 10:20:50 +0900

    test line "MainActivity" from junyoung

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    //test2 line from junyoung_test
+    //test3 line from junyoung_test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
