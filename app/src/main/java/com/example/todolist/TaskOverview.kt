7c1b1036f44a15224ff0795cc8260f95100c91d4
P-junny <skzzang0904@naver.com>
2024-10-31 01:22:23 +0900

    [Feat] 할일 activity 추가 완료

---
+++ app/src/main/java/com/example/todolist/TaskOverview.kt
@@ -0,0 +1,20 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TaskOverview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_task_overview)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.to_do_list)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
