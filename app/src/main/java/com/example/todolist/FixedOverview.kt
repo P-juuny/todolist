2347e0cb98b91eb51dd1454d605541fc6c7f0a21
P-junny <skzzang0904@naver.com>
2024-10-31 15:14:09 +0900

    버그 해결

--- app/src/main/java/com/example/todolist/FixedOverview.kt
+++ app/src/main/java/com/example/todolist/FixedOverview.kt
@@ -6,15 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class FixedOverview : AppCompatActivity() {
+/**class FixedOverview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_fixed_overview)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Fixed_Todo)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fixed_todo_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
 }
+        **/
