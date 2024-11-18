2c556ad0e4cc28d1319cee818ef2bfc0cacc7a71
KS <bubbleguy10@gmail.com>
2024-11-18 22:54:24 +0900

    hotfix start~

--- app/src/main/java/com/example/todolist/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/todolist/viewmodel/TodoViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import com.example.todolist.model.TaskItem
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
