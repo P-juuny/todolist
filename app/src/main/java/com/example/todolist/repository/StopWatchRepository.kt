a0a6fdaf1f1574819ff2f0fb258143fe63204050
wnsdud <cccjj3829@gmail.com>
2024-11-21 00:16:20 +0900

    modify Locale to Korea in StopWatchRepository

--- app/src/main/java/com/example/todolist/repository/StopWatchRepository.kt
+++ app/src/main/java/com/example/todolist/repository/StopWatchRepository.kt
@@ -12,6 +12,7 @@ import com.google.firebase.database.database
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 class StopWatchRepository {
     private val auth = Firebase.auth
@@ -97,7 +98,11 @@ class StopWatchRepository {
     }
 
     fun getCurrentDate(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        TimeZone.setDefault(timeZone)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).apply {
+            this.timeZone = timeZone
+        }
         return dateFormat.format(Date())
     }
 }
