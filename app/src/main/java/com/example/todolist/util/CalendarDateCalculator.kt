00374eb553b4e43740997fdfa7d3ab57f697cc0a
KS <bubbleguy10@gmail.com>
2024-12-14 21:37:11 +0900

    CalendarDateCalculator 최적화

--- app/src/main/java/com/example/todolist/util/CalendarDateCalculator.kt
+++ app/src/main/java/com/example/todolist/util/CalendarDateCalculator.kt
@@ -7,24 +7,24 @@ class CalendarDateCalculator {
     fun calculateMonthDates(yearMonth: YearMonth): List<DayInfo> {
         val firstDay = yearMonth.atDay(1)
         val lastDay = yearMonth.atEndOfMonth()
-        val firstDayOfWeek = firstDay.dayOfWeek.value
 
-        val startDate = firstDay.minusDays((firstDayOfWeek - 1).toLong())
-        val endDate = lastDay.plusDays((7 - lastDay.dayOfWeek.value).toLong())
+        // 시작일과 종료일 계산을 더 명확하게
+        val daysToSubtract = (firstDay.dayOfWeek.value - 1).toLong()
+        val daysToAdd = (7 - lastDay.dayOfWeek.value).toLong()
 
-        val dayInfoList = mutableListOf<DayInfo>()
-        var currentDate = startDate
+        val startDate = firstDay.minusDays(daysToSubtract)
+        val endDate = lastDay.plusDays(daysToAdd)
 
-        while (!currentDate.isAfter(endDate)) {
-            dayInfoList.add(
-                DayInfo(
+        // ArrayList로 초기 용량 설정하여 메모리 재할당 방지
+        return ArrayList<DayInfo>((42)).apply {
+            var currentDate = startDate
+            while (!currentDate.isAfter(endDate)) {
+                add(DayInfo(
                     date = currentDate,
                     isCurrentMonth = currentDate.month == yearMonth.month
-                )
-            )
-            currentDate = currentDate.plusDays(1)
+                ))
+                currentDate = currentDate.plusDays(1)
+            }
         }
-
-        return dayInfoList
     }
 }
