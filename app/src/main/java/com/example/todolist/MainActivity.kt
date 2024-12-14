ea615763a130de9d13c4ea778a8db427e7a9b831
KS <bubbleguy10@gmail.com>
2024-12-14 21:41:48 +0900

    MainActivity 최적화

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -14,7 +14,7 @@ import com.google.firebase.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
